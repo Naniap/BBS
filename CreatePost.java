9084a52679bd27e2608641a5b789fea2716c9b6a
DMillan96 <danmil114@gmail.com>
2016-12-12 12:58:32 -0500

    Add files via upload

---
+++ CreatePost.java
@@ -0,0 +1,117 @@
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.IOException;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class CreatePost implements MouseListener,ActionListener
+{
+	JFrame frame;
+	
+	ImageIcon newMessage = new ImageIcon("NewMessage.jpg");
+	ImageIcon back = new ImageIcon("Back.png");
+    ImageIcon backpressed = new ImageIcon("Back_Pressed.png");
+	
+	JLabel backButton;
+	JLabel backPressed;
+	
+	JButton post;
+	public CreatePost()
+	{
+		frame = new JFrame();
+		frame.setLocation(0, 0);
+		frame.setResizable(false);
+		frame.setTitle("Bulletin Board Client");
+		frame.setContentPane(makePanel());
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	private JPanel makePanel() 
+	{
+		JPanel panel = new JPanel();
+		panel.setPreferredSize(new Dimension(1173,780));
+		panel.setLayout(null);
+		
+		backButton = new JLabel(back);
+		backButton.setBounds(25, 650, 186, 80);
+		backButton.addMouseListener(this);
+		panel.add(backButton);
+	
+		backPressed = new JLabel(backpressed);
+		backPressed.setBounds(25, 650, 186, 80);
+		panel.add(backPressed);
+		
+		JLabel CreatePostBackground = new JLabel(newMessage);
+		CreatePostBackground.setBounds(0, 0, 1173, 795);
+		panel.add(CreatePostBackground);
+		// TODO Auto-generated method stub
+		return panel;
+	}
+	@Override
+	public void mouseClicked(MouseEvent a) 
+	{
+		// TODO Auto-generated method stub
+		if ((a.getSource() == backPressed ) 
+				|| (a.getSource() == backButton))
+		{
+			try 
+			{
+				new MainMenu();
+			} 
+			catch (IOException e1) 
+			{
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent b) 
+	{
+		// TODO Auto-generated method stub
+		if (b.getSource() == backButton)
+		{
+			backButton.setVisible(false);
+			backPressed.setVisible(true);
+		}
+	}
+	@Override
+	public void mouseExited(MouseEvent c) 
+	{
+		// TODO Auto-generated method stub
+		if (c.getSource() == backButton)
+		{
+			backButton.setVisible(true);
+			backPressed.setVisible(false);
+		}
+	}
+	@Override
+	public void mousePressed(MouseEvent arg0) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
---
+++ MainMenu.java
@@ -0,0 +1,142 @@
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.IOException;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.border.Border;
+
+
+public class MainMenu implements MouseListener,ActionListener
+{
+	JFrame frame;
+	
+	ImageIcon homescreen = new ImageIcon("HomeScreen.png");
+	ImageIcon LogOut = new ImageIcon("LogOut.png");
+	ImageIcon NewMessage = new ImageIcon("new-message.png");
+	ImageIcon DisplayAllPosts = new ImageIcon("display-all.png");
+	
+	JLabel newMessage;
+	JLabel displayAllPosts;
+	
+	JButton logOutButton;
+	
+	public MainMenu() throws IOException
+	{
+		frame = new JFrame();
+		frame.setLocation(0, 0);
+		frame.setResizable(false);
+		frame.setTitle("Bulletin Board Client");
+		frame.setContentPane(makePanel());
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+	private JPanel makePanel() throws IOException
+	{
+		JPanel panel = new JPanel();
+		panel.setPreferredSize(new Dimension(1173,780));
+		panel.setLayout(null);
+		
+		newMessage = new JLabel(NewMessage);
+		newMessage.setBounds(60, 75, 264, 237);
+		newMessage.addMouseListener(this);
+		panel.add(newMessage);
+		
+		displayAllPosts = new JLabel(DisplayAllPosts);
+		displayAllPosts.setBounds(250, 50, 275, 250);
+		displayAllPosts.addMouseListener(this);
+		panel.add(displayAllPosts);
+		
+		logOutButton = new JButton(LogOut);
+		logOutButton.setBounds(47, 685, 186, 49);
+		logOutButton.addActionListener(this);
+		panel.add(logOutButton);
+		
+		JLabel MainMenuBackground = new JLabel(homescreen);
+		MainMenuBackground.setBounds(0, 0, 1173, 795);
+		panel.add(MainMenuBackground);
+		
+		return panel;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) 
+	{
+		if (e.getSource() == newMessage)
+		{
+			new CreatePost();
+			frame.setVisible(false);
+		}
+		else if (e.getSource() == displayAllPosts)
+		{
+			try 
+			{
+				new BillBoardClient("127.0.0.1");
+			} 
+			catch (IOException a) 
+			{
+				// TODO Auto-generated catch block
+				a.printStackTrace();
+			}
+			frame.setVisible(false);
+		}
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) 
+	{
+		
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent a) 
+	{
+		// TODO Auto-generated method stub
+		if (a.getSource() == logOutButton)
+		{
+			try 
+			{
+				new BillBoardClient("127.0.0.1");
+			} 
+			catch (IOException e) 
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			frame.setVisible(false);
+			
+		}
+	}
+}
